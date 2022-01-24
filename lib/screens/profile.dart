acb96b3c0c2d81dcaab2a452e5c77ed50a8b17e1
Majestr32 <61013205+Majestr32@users.noreply.github.com>
2022-01-24 17:44:18 +0200

    Updated upper profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -12,74 +12,156 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: [
-            _firstWidget(),
-            SizedBox(height: 20,),
-            _secondWidget(),
-            Spacer(),
-            _thirdWidget(),
-
-
-          ],
-        )
-    );
+      children: [
+        _firstWidget(),
+        SizedBox(
+          height: 20,
+        ),
+        Spacer(),
+        _thirdWidget(),
+      ],
+    ));
   }
 
   Widget _firstWidget() {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 320,
-        decoration: BoxDecoration(
-            color: Color(0xffB8BFE4),
-            borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(200,50))
-        ),
-        child: Column(
-          children: [
-            Container(
-                child: Image(
-                  image: AssetImage("assets/ProfilePhoto.png"),
-                  width: 120,
-                  height: 120,
-                ),
-                margin: EdgeInsets.only(top: 32)
+    return Container(
+      height: 350,
+      child: Stack(
+        children: [
+          _bottomOfTopBar(),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 270,
+              decoration: BoxDecoration(
+                  color: Color(0xFF5772FF),
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.elliptical(200, 50))),
+              child: Column(
+                children: [
+                  Container(
+                      child: Image(
+                        image: AssetImage("assets/ProfilePhoto.png"),
+                        width: 120,
+                        height: 120,
+                      ),
+                      margin: EdgeInsets.only(top: 32)),
+                  SizedBox(height: 20),
+                  Stack(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Dan Brandon",
+                              style: TextStyle(
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "DanBrandon.02",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontStyle: FontStyle.italic,
+                                  color: Color(0xFF1F1F1F)),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      right: 50,
+                      top: 15,
+                      child: Icon(
+                        Icons.mode_edit,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ),
+                  ]),
+                ],
+              ),
             ),
-            SizedBox(height: 20),
-            Text("Dan Brandon",
-              style: TextStyle(
-                  fontSize: 26, fontWeight: FontWeight.bold
-              ),),
-            SizedBox(height: 8,),
-            Text("DanBrandon.02", style: TextStyle(fontSize: 18),),
-            SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(FontAwesomeIcons.facebook, color: Color(0xff000000),),
-                SizedBox(width: 20,),
-                Icon(FontAwesomeIcons.telegram, color: Color(0xff000000),),
-              ],
-            ),
-
-          ],
-        ),
+          ),
+          Positioned(
+              right: 30,
+              top: 30,
+              child: Icon(Icons.settings, color: Colors.white, size: 32)),
+        ],
       ),
     );
   }
-
+  Widget _bottomOfTopBar(){
+    return Align(alignment: Alignment.bottomCenter,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Color(0xFFFFC044),
+            borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(200, 50))),
+        width: MediaQuery.of(context).size.width,
+        height: 120,
+        child: _secondWidget(),
+      ),);
+  }
   Widget _secondWidget() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text("Quizzes ended", style: TextStyle(fontSize: 19),),
-          Text("Followers", style: TextStyle(fontSize: 19),),
-          Text("Following", style: TextStyle(fontSize: 19),)
-        ],
-
-
+      child: Container(
+        margin: EdgeInsets.only(top: 33),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text(
+                  "Followers",
+                  style: TextStyle(fontSize: 19),
+                ),
+                Text(
+                  "23",
+                  style: TextStyle(fontSize: 19,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Column(
+                children: [
+                  Text(
+                    "Quizes passed",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  Text(
+                    "13",
+                    style: TextStyle(fontSize: 19,fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Followings",
+                  style: TextStyle(fontSize: 19,),
+                ),
+                Text(
+                  "56",
+                  style: TextStyle(fontSize: 19,
+                  fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -90,19 +172,16 @@ class _ProfilePageState extends State<ProfilePage> {
       child: Container(
         margin: EdgeInsets.only(bottom: 16),
         child: ElevatedButton(
-          onPressed: () {  },
+          onPressed: () {},
           style: ElevatedButton.styleFrom(
-              fixedSize: Size(200,50),
+              fixedSize: Size(200, 50),
               primary: Color(0xffB8BFE4),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18)
-              )
+                  borderRadius: BorderRadius.circular(18))),
+          child: Text(
+            "Log out",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
           ),
-          child: Text("Log out",
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.normal
-            ),),
         ),
       ),
     );
